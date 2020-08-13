132c97997a65b8ad95d3355bfc2b7e08e682d907
vaishakchanguli <41374497+vaishakchanguli@users.noreply.github.com>
2020-08-13 20:07:16 +0530

    Update and rename table.component.ts to grid.component.ts

---
+++ src/app/widgets/table/grid.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core'
+
+@Component({
+selector: 'grid', 
+templateUrl: './grid.component.html',
+styleUrls:['./grid.component.css']
+})
+export class GridComponent{
+  
+
+}
